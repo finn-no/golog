18ef7a0f52cff577f2967c571a7b31b240f4ac6b
Jamie Wiebe <jamie.wiebe@schibsted.com>
2022-08-08 15:42:47 +0200

    Remove intermediary log functions
    When this package's intermediary functions (Infof, Debug, etc) are
    called, the resulting log line's caller field is set to this package.
    That should be set to the actual caller, so this way of logging should
    be removed.

--- log.go
+++ log.go
@@ -45,39 +45,3 @@ func SetLevel(levelStr string) error {
 func Level() string {
 	return logger.Level.String()
 }
-
-// Debug proxies logrus.Logger.Debug
-func Debug(args ...interface{}) { logger.Debug(args...) }
-
-// Debugf proxies logrus.Logger.Debugf
-func Debugf(format string, args ...interface{}) { logger.Debugf(format, args...) }
-
-// Error proxies logrus.Logger.Error
-func Error(args ...interface{}) { logger.Error(args...) }
-
-// Errorf proxies logrus.Logger.Errorf
-func Errorf(format string, args ...interface{}) { logger.Errorf(format, args...) }
-
-// Fatal proxies logrus.Logger.Fatal
-func Fatal(args ...interface{}) { logger.Fatal(args...) }
-
-// Fatalf proxies logrus.Logger.Fatalf
-func Fatalf(format string, args ...interface{}) { logger.Fatalf(format, args...) }
-
-// Info proxies logrus.Logger.Info
-func Info(args ...interface{}) { logger.Info(args...) }
-
-// Infof proxies logrus.Logger.Infof
-func Infof(format string, args ...interface{}) { logger.Infof(format, args...) }
-
-// Trace proxies logrus.Logger.Trace
-func Trace(args ...interface{}) { logger.Trace(args...) }
-
-// Tracef proxies logrus.Logger.Tracef
-func Tracef(format string, args ...interface{}) { logger.Tracef(format, args...) }
-
-// Warn proxies logrus.Logger.Warn
-func Warn(args ...interface{}) { logger.Warn(args...) }
-
-// Warnf proxies logrus.Logger.Warnf
-func Warnf(format string, args ...interface{}) { logger.Warnf(format, args...) }
